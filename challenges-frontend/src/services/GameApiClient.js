cb95556e8647a8b304cc502df645e7870bf58dee
Tarun Telang <tarun.telang@gmail.com>
2023-12-03 09:50:52 +0530

    Update GameApiClient.js

--- challenges-frontend/src/services/GameApiClient.js
+++ challenges-frontend/src/services/GameApiClient.js
@@ -1,5 +1,5 @@
 class GameApiClient {
-    static SERVER_URL = 'https://cautious-system-p7j5qp97vcrg6v-8000.app.github.dev';
+    static SERVER_URL = 'http://localhost:8000';
     static GET_LEADERBOARD = '/leaders';
 
     static leaderBoard(): Promise<Response> {
